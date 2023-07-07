1587431bc582e0d9b41d98cb9cec11838c4bed6e
Zibetnu <9at25jnr3@mozmail.com>
2023-07-07 17:10:57 -0600

    Revert animation name if new name does not exist

--- src/player/costume/costume.gd
+++ src/player/costume/costume.gd
@@ -11,8 +11,10 @@ const DIRECTION_NAMES = {
 
 var animation_name := "idle":
 	set(value):
+		var old_animation_name = animation_name
 		animation_name = value
-		_update_animation()
+		if not _update_animation():
+			animation_name = old_animation_name
 
 var direction_vector := Vector2.DOWN:
 	set(value):
@@ -31,20 +33,21 @@ func _get_direction_name(direction: Vector2) -> String:
 	return DIRECTION_NAMES[snapped_vector]
 
 
-func _update_animation() -> void:
+func _update_animation() -> bool:
 	var directional_animation_name := "%s_%s" % [
 			animation_name, _get_direction_name(direction_vector)
 	]
 
 	if directional_animation_name == $AnimationPlayer.current_animation:
-		return
+		return true
 
 	if $AnimationPlayer.has_animation(directional_animation_name):
 		$AnimationPlayer.play(directional_animation_name)
-		return
+		return true
 
 	if $AnimationPlayer.has_animation(animation_name):
 		$AnimationPlayer.play(animation_name)
-		return
+		return true
 
+	return false
 
