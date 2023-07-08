ddc4d69a1a53f8f007000774d0226f799245c3d4
Zibetnu <9at25jnr3@mozmail.com>
2023-07-07 18:12:37 -0600

    Save and load network settings

--- src/main_menu/main_menu.gd
+++ src/main_menu/main_menu.gd
@@ -1,12 +1,36 @@
 extends Control
 
 
+const SECTION = "network"
+const IP_KEY = "ip_address"
+const PORT_KEY = "port"
+
+
 func _ready() -> void:
+	_load_address()
 	%HostButton.grab_focus()
 
 
 func start_lobby() -> void:
+	_save_address()
 	SceneChanger.change_scene_to_packed(SceneChanger.lobby)
+
+
+func _load_address() -> void:
+	var config := ConfigFile.new()
+	if config.load(ProjectSettings.get_setting("global/settings_file_path")) != OK:
+		return
+
+	if not config.has_section(SECTION):
+		return
+
+	if config.has_section_key(SECTION, IP_KEY):
+		%IPText.text = config.get_value(SECTION, IP_KEY)
+
+	if config.has_section_key(SECTION, PORT_KEY):
+		var port: int = config.get_value(SECTION, PORT_KEY)
+		if port != 0:
+			%PortText.text = str(port)
 
 
 func _on_host_button_pressed() -> void:
@@ -17,3 +41,10 @@ func _on_host_button_pressed() -> void:
 func _on_join_button_pressed() -> void:
 	ConnectionManager.create_client(%IPText.text, int(%PortText.text))
 	start_lobby()
+
+
+func _save_address() -> void:
+	var config := ConfigFile.new()
+	config.set_value(SECTION, IP_KEY, %IPText.text)
+	config.set_value(SECTION, PORT_KEY, int(%PortText.text))
+	config.save(ProjectSettings.get_setting("global/settings_file_path"))
