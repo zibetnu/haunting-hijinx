16c5b72702d256f05f1a2dd284ad890862da94f8
Zibetnu <9at25jnr3@mozmail.com>
2023-07-26 02:34:00 +0000

    Overhaul drain area behavior and visuals

--- src/drain_area/drain_area.gd
+++ src/drain_area/drain_area.gd
@@ -3,33 +3,60 @@ extends Area2D
 
 @export var damage_source: DamageSource
 
-var _affected := []
+var _active := false
+var _affected_collision_objects: Array[CollisionObject2D] = []
+
+
+func _damage_affected_collision_objects() -> void:
+	if not _active:
+		return
+
+	for collision_object in _affected_collision_objects:
+		if not collision_object.has_method("take_damage"):
+			continue
+
+		collision_object.take_damage(damage_source)
+		if multiplayer.is_server():
+			queue_free()
 
 
 func _on_area_entered(area: Area2D) -> void:
-	if not area in _affected:
-		_affected.append(area)
+	_on_entered(area)
 
 
 func _on_area_exited(area: Area2D) -> void:
-	_affected.erase(area)
+	_on_exited(area)
 
 
-func _on_body_entered(body: Node2D) -> void:
-	if not body in _affected:
-		_affected.append(body)
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	_on_entered(body)
 
 
-func _on_body_exited(body: Node2D) -> void:
-	_affected.erase(body)
+func _on_body_exited(body: PhysicsBody2D) -> void:
+	_on_exited(body)
 
 
-func _on_timer_timeout() -> void:
-	for node in _affected:
-		if node.has_method("take_damage"):
-			node.take_damage(damage_source)
-
-	if not multiplayer.is_server():
+func _on_entered(collision_object: CollisionObject2D) -> void:
+	if collision_object in _affected_collision_objects:
 		return
 
-	queue_free()
+	_affected_collision_objects.append(collision_object)
+	_damage_affected_collision_objects()
+
+
+func _on_exited(collision_object: CollisionObject2D) -> void:
+	_affected_collision_objects.erase(collision_object)
+
+
+func _on_idle_timer_timeout() -> void:
+	_active = true
+	_damage_affected_collision_objects()
+	$Foreground.material.set_shader_parameter("amplitude", 2)
+	$Foreground.material.set_shader_parameter("frequency", 4)
+	$Foreground.material.set_shader_parameter("ripple_rate", 12)
+	$ActiveTimer.start()
+
+
+func _on_active_timer_timeout() -> void:
+	if multiplayer.is_server():
+		queue_free()
