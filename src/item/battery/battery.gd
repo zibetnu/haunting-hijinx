6e6415e35b78d71c785e7e678df06122ce3bdc02
Zibetnu <9at25jnr3@mozmail.com>
2023-09-16 17:16:14 -0600

    Make batteries destructible by drain areas

--- src/item/battery/battery.gd
+++ src/item/battery/battery.gd
@@ -1,10 +1,26 @@
 extends Item
 
 
+@export var weak_to: DamageSource.Type
+
+
 func _ready() -> void:
 	$AnimationPlayer.play("spawn")
 	await $AnimationPlayer.animation_finished
 	$AnimationPlayer.play("bob")
+
+
+func take_damage(source: DamageSource) -> void:
+	if source.damage_type != weak_to:
+		return
+
+	$AnimationPlayer.play("destroy")
+	if not multiplayer.is_server():
+		return
+
+	$CollisionShape2D.disabled = true
+	await $AnimationPlayer.animation_finished
+	queue_free()
 
 
 func _acquirer_allowed(acquirer: Variant) -> bool:
