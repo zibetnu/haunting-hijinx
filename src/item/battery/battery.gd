5d007b75d9e4cd16e8fded9eeabcd92277f90375
Zibetnu <9at25jnr3@mozmail.com>
2023-09-24 11:08:59 -0600

    Fix battery destruction not syncing

--- src/item/battery/battery.gd
+++ src/item/battery/battery.gd
@@ -10,17 +10,21 @@ func _ready() -> void:
 	$AnimationPlayer.play("bob")
 
 
+@rpc("call_local")
+func destoy() -> void:
+	$AnimationPlayer.play("destroy")
+	$CollisionShape2D.disabled = true
+	await $AnimationPlayer.animation_finished
+	if multiplayer.is_server():
+		queue_free()
+
+
 func take_damage(source: DamageSource) -> void:
 	if source.damage_type != weak_to:
 		return
 
-	$AnimationPlayer.play("destroy")
-	if not multiplayer.is_server():
-		return
-
-	$CollisionShape2D.disabled = true
-	await $AnimationPlayer.animation_finished
-	queue_free()
+	if multiplayer.is_server():
+		destoy.rpc()
 
 
 func _acquirer_allowed(acquirer: Variant) -> bool:
