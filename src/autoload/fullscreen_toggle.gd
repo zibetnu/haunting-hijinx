e76c7d16a4adce1503e6d91500e7e93971094f43
Zibetnu <9at25jnr3@mozmail.com>
2023-11-17 19:57:26 -0700

    Use exclusive fullscreen

--- src/autoload/fullscreen_toggle.gd
+++ src/autoload/fullscreen_toggle.gd
@@ -9,8 +9,8 @@ func _input(event: InputEvent) -> void:
 	if not event.is_action_pressed("toggle_fullscreen"):
 		return
 
-	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
