9f7650530114e686f58b8d3b310e4ec743cc1d18
Zibetnu <9at25jnr3@mozmail.com>
2023-12-06 11:35:35 -0700

    Fix notifications jittering while player is moving

--- src/player/ability/invisible/invisible.gd
+++ src/player/ability/invisible/invisible.gd
@@ -7,15 +7,19 @@ var active := false:
 			return
 
 		active = value
+		var make_visible := false
 		if active:
 			player.position_public_visibility = false
 			player.modulate.a = 0.5
-			player.visible = player.peer_id == multiplayer.get_unique_id()
+			make_visible = player.peer_id == multiplayer.get_unique_id()
 
 		else:
 			player.position_public_visibility = true
 			player.modulate.a = 1
-			player.visible = true
+			make_visible = true
+
+		player.visible = make_visible
+		player.get_node("IgnoreCanvasModulate").visible = make_visible
 
 
 func _on_player_state_machine_transitioned(state_name: String) -> void:
