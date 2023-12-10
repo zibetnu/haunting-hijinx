2b466a8553ba2b04059beff04c3366e9a5cf6e0a
Zibetnu <9at25jnr3@mozmail.com>
2023-12-10 10:42:29 -0700

    Fix auto bar crash if variables are not set

--- src/auto_texture_progress_bar/auto_texture_progress_bar.gd
+++ src/auto_texture_progress_bar/auto_texture_progress_bar.gd
@@ -1,18 +1,35 @@
 extends TextureProgressBar
 
 
-@export var source_node: Node
-@export var value_variable: String
-@export var max_variable: String
+@export var source_node: Node:
+	set(value):
+		source_node = value
+		_check_variables()
+
+@export var value_variable: String:
+	set(value):
+		value_variable = value
+		_check_variables()
+
+@export var max_variable: String:
+	set(value):
+		max_variable = value
+		_check_variables()
 
 
 func _ready() -> void:
-	set_process(
+	_check_variables()
+
+
+func _check_variables() -> void:
+	var all_valid := (
 			source_node != null
 			and source_node.get(value_variable) != null
 			and source_node.get(max_variable) != null
 	)
-	max_value = source_node.get(max_variable)
+	set_process(all_valid)
+	if all_valid:
+		max_value = source_node.get(max_variable)
 
 
 func _process(_delta: float) -> void:
