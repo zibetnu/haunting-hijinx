68f62c650b6e00ac0419ae806dbed098321e9790
Zibetnu <9at25jnr3@mozmail.com>
2023-12-10 16:20:03 -0700

    Refactor ping label

--- src/ping_label/ping_label.gd
+++ src/ping_label/ping_label.gd
@@ -1,16 +1,17 @@
 extends Label
 
 
-func _ready() -> void:
-	ConnectionManager.ping_set.connect(_on_ping_set)
-
-
-func _on_ping_set(ping: int) -> void:
-	text = "%s ms" % ping
-
-
 func _on_timer_timeout() -> void:
-	if multiplayer.is_server():
-		return
+	var last_rtt := 0.0
+	var last_variance := 0.0
+	var mean_rtt := 0.0
+	var mean_variance := 0.0
+	var peer := multiplayer.multiplayer_peer as ENetMultiplayerPeer
+	if not multiplayer.is_server() and peer:
+		var server := peer.get_peer(1)
+		last_rtt = server.get_statistic(ENetPacketPeer.PEER_LAST_ROUND_TRIP_TIME)
+		last_variance = server.get_statistic(ENetPacketPeer.PEER_LAST_ROUND_TRIP_TIME_VARIANCE)
+		mean_rtt = server.get_statistic(ENetPacketPeer.PEER_ROUND_TRIP_TIME)
+		mean_variance = server.get_statistic(ENetPacketPeer.PEER_ROUND_TRIP_TIME_VARIANCE)
 
-	ConnectionManager.get_ping.rpc_id(1, Time.get_ticks_usec())
+	text = "Last: %s ± %s; Mean: %s ± %s" % [last_rtt, last_variance, mean_rtt, mean_variance]
