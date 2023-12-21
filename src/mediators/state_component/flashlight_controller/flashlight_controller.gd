5fd30b44cc3a92e6464357a6519cf27914cb40e3
Zibetnu <9at25jnr3@mozmail.com>
2023-12-21 16:23:27 -0700

    Add flashlight controller

---
+++ src/mediators/state_component/flashlight_controller/flashlight_controller.gd
@@ -0,0 +1,31 @@
+extends StateComponent
+
+
+@export var flashlight_behavior: Node
+@export var controller: Controller:
+	set(value):
+		if controller and controller.input_handled.is_connected(_on_controller_input_handled):
+			controller.input_handled.disconnect(_on_controller_input_handled)
+
+		controller = value
+		if controller and not controller.input_handled.is_connected(_on_controller_input_handled):
+			controller.input_handled.connect(_on_controller_input_handled)
+
+var _active := false
+
+
+func _on_controller_input_handled() -> void:
+	if not _active:
+		return
+
+	flashlight_behavior.set("powered", controller.button_1_pressed)
+	if not controller.look_vector.is_zero_approx():
+		flashlight_behavior.set("target_rotation", controller.look_vector.angle())
+
+
+func _on_state_entered() -> void:
+	_active = true
+
+
+func _on_state_exited() -> void:
+	_active = false
