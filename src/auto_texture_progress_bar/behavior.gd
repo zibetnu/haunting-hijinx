a988e75c7662b73ce51b8a47fd9f0a21bbf7b943
Zibetnu <9at25jnr3@mozmail.com>
2024-01-08 10:28:35 -0700

    Separate behavior for auto texture progress bar

--- src/auto_texture_progress_bar/behavior.gd
+++ src/auto_texture_progress_bar/behavior.gd
@@ -1,20 +1,32 @@
-extends TextureProgressBar
+extends Node
 
 
 @export var source_node: Node:
 	set(value):
 		source_node = value
-		_check_variables()
+		if _is_ready:
+			_check_variables()
 
 @export var value_variable: String:
 	set(value):
 		value_variable = value
-		_check_variables()
+		if _is_ready:
+			_check_variables()
 
 @export var max_variable: String:
 	set(value):
 		max_variable = value
-		_check_variables()
+		if _is_ready:
+			_check_variables()
+
+var _is_ready := false
+
+@onready var bar: TextureProgressBar = get_parent()
+
+
+func _ready() -> void:
+	_is_ready = true
+	_check_variables()
 
 
 func _check_variables() -> void:
@@ -25,8 +37,8 @@ func _check_variables() -> void:
 	)
 	set_process(all_valid)
 	if all_valid:
-		max_value = source_node.get(max_variable)
+		bar.max_value = source_node.get(max_variable)
 
 
 func _process(_delta: float) -> void:
-	value = source_node.get(value_variable)
+	bar.value = source_node.get(value_variable)
