9c2641afa6566f6fc0e94cf48ebaf48460dfdfc1
Zibetnu <9at25jnr3@mozmail.com>
2024-01-11 10:06:50 -0700

    Add hunter name label

--- src/mediators/peer_id_name_label/peer_id_name_label.gd
+++ src/mediators/peer_id_name_label/peer_id_name_label.gd
@@ -10,13 +10,8 @@ extends Node
 		if peer_id:
 			peer_id.changed.connect(_on_peer_id_changed)
 
-@export var canvas_item: CanvasItem
-@export var invert := false
+@export var label: Label
 
 
 func _on_peer_id_changed(id: int) -> void:
-	if invert:
-		canvas_item.visible = not id == multiplayer.get_unique_id()
-
-	else:
-		canvas_item.visible = id == multiplayer.get_unique_id()
+	label.text = PeerData.peer_names.get(id, "")
