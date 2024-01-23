efce72c916b83bf801bf4897d1b2648c37d88de5
Zibetnu <9at25jnr3@mozmail.com>
2024-01-23 09:51:04 -0700

    Refator hunter damage handling more

--- src/mediators/damage_event/damage_event.gd
+++ src/mediators/damage_event/damage_event.gd
@@ -1,15 +1,20 @@
 extends Node
 
 
+@export var active := false
+@export var event: StringName
 @export var state_chart: StateChart
 @export var weak_to: DamageSource.Type
 
 
 func take_damage(source: DamageSource) -> void:
+	if not active:
+		return
+
 	if source.damage_type != weak_to:
 		return
 
 	if not state_chart:
 		return
 
-	state_chart.send_event("died")
+	state_chart.send_event(event)
