f4de3b48f86e4a3edbb3325c7fc43dd111a2160a
Zibetnu <9at25jnr3@mozmail.com>
2024-01-31 09:40:29 -0700

    Provent ghost being visible before its position is updated

--- src/mediators/peer_id_visibility_filter/peer_id_visibility_filter.gd
+++ src/mediators/peer_id_visibility_filter/peer_id_visibility_filter.gd
@@ -1,9 +1,20 @@
 extends Node
 
 
+signal filter_enabled
+signal filter_disabled
+
 @export var peer_id: PeerID
 @export var synchronizer: MultiplayerSynchronizer
-@export var enable_filter := true
+@export var enable_filter := true:
+	set(value):
+		enable_filter = value
+		if value:
+			filter_enabled.emit()
+
+		else:
+			filter_disabled.emit()
+
 @export var include_ghost_peer := false
 
 var _filter = func(id):
