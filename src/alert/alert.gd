2b82f4a8eea63cb42937926f7b0d405e5eb8990e
Zibetnu <9at25jnr3@mozmail.com>
2024-02-01 10:45:10 -0700

    Replace AuraSenseAlert with methods and signals

--- src/alert/alert.gd
+++ src/alert/alert.gd
@@ -24,6 +24,21 @@ func _ready() -> void:
 	_is_ready = true
 
 
+func set_alert_level(value: AlertLevel) -> void:
+	alert_level = value
+
+
+func set_alert_level_from_int(value: int) -> void:
+	if value <= 0:
+			alert_level = AlertLevel.NONE
+
+	elif value == 1:
+		alert_level = AlertLevel.LOW
+
+	else:
+		alert_level = AlertLevel.HIGH
+
+
 func _update_animation() -> void:
 	match alert_level:
 		AlertLevel.NONE:
