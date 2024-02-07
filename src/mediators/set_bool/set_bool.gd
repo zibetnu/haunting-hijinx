c011f7ce87ce456bcb1d7fce9e74be4c473e14b3
Zibetnu <9at25jnr3@mozmail.com>
2024-02-07 13:52:18 -0700

    Add deferred methods to SetBool

--- src/mediators/set_bool/set_bool.gd
+++ src/mediators/set_bool/set_bool.gd
@@ -13,5 +13,13 @@ func set_false() -> void:
 	target_node.set(bool_name, false)
 
 
+func set_false_deferred() -> void:
+	target_node.set_deferred(bool_name, false)
+
+
 func set_true() -> void:
 	target_node.set(bool_name, true)
+
+
+func set_true_deferred() -> void:
+	target_node.set_deferred(bool_name, true)
