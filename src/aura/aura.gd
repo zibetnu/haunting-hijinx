ef57777a027203c0921c5f30a1b2681ad73401f0
Zibetnu <9at25jnr3@mozmail.com>
2024-02-09 20:46:28 -0700

    Revert "Add aura outlines again"
    This reverts commit ad546b1426a70222e1c4178711688ff82922b7ea.

--- src/aura/aura.gd
+++ src/aura/aura.gd
@@ -2,23 +2,6 @@ extends Area2D
 
 
 @export var intensity := 1
-@export var outline_visible := false:
-	set(value):
-		outline_visible = value
-		queue_redraw()
-
-
-func _draw() -> void:
-	var radius = $CollisionShape2D.shape.get("radius")
-	draw_arc(position, radius if radius != null else 10, 0, 2 * PI, 32, Color8(255, 255, 255, 63))
-
-
-func hide_outline() -> void:
-	outline_visible = false
-
-
-func show_outline() -> void:
-	outline_visible = true
 
 
 func _on_entered(node: Node) -> void:
