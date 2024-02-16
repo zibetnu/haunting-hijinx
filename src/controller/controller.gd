b37dc7242772a0f2e096e963c4a7a4e1167a50c2
Zibetnu <9at25jnr3@mozmail.com>
2024-02-16 10:28:21 -0700

    Refactor Move StateComponent

--- src/controller/controller.gd
+++ src/controller/controller.gd
@@ -3,8 +3,15 @@ extends Node2D
 
 
 signal input_handled
+signal move_vector_changed(move_vector: Vector2)
+signal move_vector_changed_normalized(move_vector: Vector2)
 
-@export var move_vector := Vector2.ZERO
+@export var move_vector := Vector2.ZERO:
+	set(value):
+		move_vector = value
+		move_vector_changed.emit(move_vector)
+		move_vector_changed_normalized.emit(move_vector.normalized())
+
 @export var look_vector := Vector2.ZERO
 @export var button_1_pressed := false
 @export var button_2_pressed := false
--- src/mediators/state_component/move/move.gd
+++ src/mediators/state_component/move/move.gd
@@ -1,11 +1,28 @@
-extends StateComponent
+extends Node
 
 
-@export var controller: Controller
 @export var character: CharacterBody2D
 @export var speed := 128.0
+@export var manual_process := false:
+	set(value):
+		manual_process = value
+		set_physics_process(not manual_process)
+
+var move_vector: Vector2
 
 
-func _on_state_physics_processing(_delta: float) -> void:
-	character.velocity = controller.move_vector.normalized() * speed
+func _ready() -> void:
+	set_physics_process(not manual_process)
+
+
+func _physics_process(delta: float) -> void:
+	manual_physics_process(delta)
+
+
+func manual_physics_process(_delta: float) -> void:
+	character.velocity = move_vector * speed
 	character.move_and_slide()
+
+
+func set_move_vector(value: Vector2) -> void:
+	move_vector = value
