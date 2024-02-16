7d8e0fcea56dfd07c7b97def562f86461736e65c
Zibetnu <9at25jnr3@mozmail.com>
2024-02-16 09:45:53 -0700

    Replace Animation StateComponent

--- src/mediators/state_component/animation/animation.gd
+++
@@ -1,9 +0,0 @@
-extends StateComponent
-
-
-@export var animation_player: AnimationPlayer
-@export var animation_name: StringName
-
-
-func _on_state_entered() -> void:
-	animation_player.play(animation_name)
