ef82a6cbc2b90e3721b365f50de9ee4fd966702b
Zibetnu <9at25jnr3@mozmail.com>
2024-02-24 11:35:49 -0700

    Replace ControllerStateChartEvents

--- src/controller/controller.gd
+++ src/controller/controller.gd
@@ -3,16 +3,33 @@ extends Node2D
 
 
 signal button_1_changed(button_1: bool)
+signal button_1_pressed
+signal button_1_released
 signal button_2_changed(button_1: bool)
+signal button_2_pressed
+signal button_2_released
 signal input_handled
+signal move_started
+signal move_stopped
 signal move_vector_changed(move_vector: Vector2)
 signal look_vector_changed(look_vector: Vector2)
 
-
 @export var move_vector := Vector2.ZERO:
 	set(value):
-		move_vector = value.normalized()
+		value = value.normalized()
+		var vector_state: Array[bool] = [
+			move_vector.is_equal_approx(value),
+			move_vector.is_zero_approx(),
+			value.is_zero_approx(),
+		]
+		move_vector = value
 		move_vector_changed.emit(move_vector)
+		match vector_state:
+			[false, false, true]:
+				move_stopped.emit()
+
+			[false, true, false]:
+				move_started.emit()
 
 @export var look_vector := Vector2.ZERO:
 	set(value):
@@ -21,13 +38,27 @@ signal look_vector_changed(look_vector: Vector2)
 
 @export var button_1 := false:
 	set(value):
+		var button_state: Array[bool] = [button_1, value]
 		button_1 = value
 		button_1_changed.emit(button_1)
+		match button_state:
+			[true, false]:
+				button_1_released.emit()
+
+			[false, true]:
+				button_1_pressed.emit()
 
 @export var button_2 := false:
 	set(value):
+		var button_state: Array[bool] = [button_2, value]
 		button_2 = value
 		button_2_changed.emit(button_2)
+		match button_state:
+			[true, false]:
+				button_2_released.emit()
+
+			[false, true]:
+				button_2_pressed.emit()
 
 
 func force_handle_input() -> void:
