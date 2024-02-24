68f923593206a2ad7e604495855834f557c83b32
Zibetnu <9at25jnr3@mozmail.com>
2024-02-23 17:02:47 -0700

    Add signals for when buttons 1 and 2 change

--- src/controller/controller.gd
+++ src/controller/controller.gd
@@ -2,6 +2,8 @@ class_name Controller
 extends Node2D
 
 
+signal button_1_changed(button_1: bool)
+signal button_2_changed(button_1: bool)
 signal input_handled
 signal move_vector_changed(move_vector: Vector2)
 signal look_vector_changed(look_vector: Vector2)
@@ -17,8 +19,15 @@ signal look_vector_changed(look_vector: Vector2)
 		look_vector = value.normalized()
 		look_vector_changed.emit(look_vector)
 
-@export var button_1 := false
-@export var button_2 := false
+@export var button_1 := false:
+	set(value):
+		button_1 = value
+		button_1_changed.emit(button_1)
+
+@export var button_2 := false:
+	set(value):
+		button_2 = value
+		button_2_changed.emit(button_2)
 
 
 func force_handle_input() -> void:
