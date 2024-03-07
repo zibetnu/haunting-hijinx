a71f54a6075a8097387db60720c0fa2ba2b625fe
Zibetnu <9at25jnr3@mozmail.com>
2024-03-07 09:49:12 -0700

    Add ServerRelayDisabler

--- src/autoload/connection_manager/connection_manager.gd
+++ src/autoload/connection_manager/connection_manager.gd
@@ -2,8 +2,6 @@ extends Node
 
 
 func _ready() -> void:
-	# Disable server relay to ensure that data is only shared between clients when necessary.
-	multiplayer.server_relay = false
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
 
 
