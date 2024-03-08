5b52b8f8165c60fe0a92cd356ddd519b1475bf44
Zibetnu <9at25jnr3@mozmail.com>
2024-03-08 10:12:20 -0700

    Add dialog for scene changer handling disconnect

--- src/autoload/scene_changer/scene_changer.gd
+++ src/autoload/scene_changer/scene_changer.gd
@@ -4,9 +4,11 @@ extends Node
 @export var lobby: PackedScene
 @export var main_menu: PackedScene
 
+@onready var disconnected_dialog: AcceptDialog = $DisconnectedDialog
+
 
 func _ready() -> void:
-	multiplayer.server_disconnected.connect(func(): change_scene_to_packed(main_menu))
+	multiplayer.server_disconnected.connect(_on_server_disconnected)
 
 
 func change_scene_to_file(path: String) -> void:
@@ -23,3 +25,9 @@ func remove_scene() -> void:
 	for child in $SceneContainer.get_children():
 		$SceneContainer.remove_child(child)
 		child.queue_free()
+
+
+func _on_server_disconnected() -> void:
+	multiplayer.multiplayer_peer = OfflineMultiplayerPeer.new()
+	disconnected_dialog.popup_centered()
+	change_scene_to_packed(main_menu)
