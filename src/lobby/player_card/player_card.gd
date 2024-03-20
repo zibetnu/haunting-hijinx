a2a978fda1c7e32d9eb42dd296a629a72ced61c2
Zibetnu <9at25jnr3@mozmail.com>
2024-03-20 11:12:46 -0600

    Fix peer names being reset after return to lobby

--- src/lobby/player_card/player_card.gd
+++ src/lobby/player_card/player_card.gd
@@ -3,6 +3,9 @@ extends PanelContainer
 
 signal player_name_changed(value: String)
 
+const AUTOLOAD_NAMES_PROPERTY := &"peer_names"
+const AUTOLOAD_PATH := ^"/root/PeerData"
+
 @export var input_authority := 1:
 	set(value):
 		input_authority = value
@@ -18,16 +21,29 @@ var player_name: String:
 
 func _ready() -> void:
 	PeerData.peer_participation_changed.connect(_on_peer_participation_changed)
-	player_name = "Player %s" % str(multiplayer.get_unique_id())
-	%NameLineEdit.text = player_name
+	set_player_name(str(_get_autoload_peer_name(input_authority)), true)
 	%NameLineEdit.visible = multiplayer.get_unique_id() == input_authority
 	%NameLabel.visible = not %NameLineEdit.visible
 	%ParticipationToggle.visible = multiplayer.is_server()
 	_on_peer_participation_changed(input_authority)
 
 
-func set_player_name(value: String) -> void:
+func set_player_name(value: String, update_line_edit := false) -> void:
 	player_name = value
+	if update_line_edit:
+		%NameLineEdit.text = player_name
+
+
+func _get_autoload_peer_name(id: int) -> Variant:
+	var autoload := get_node_or_null(AUTOLOAD_PATH)
+	if not autoload:
+		return null
+
+	var peer_names = autoload.get(AUTOLOAD_NAMES_PROPERTY)
+	if not peer_names is Dictionary:
+		return null
+
+	return peer_names.get(id)
 
 
 func _on_participation_toggle_pressed() -> void:
