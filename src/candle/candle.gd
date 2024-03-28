9d0cee3b91287d15c5e7c0e40e5456a3449f3975
Zibetnu <9at25jnr3@mozmail.com>
2024-03-28 10:17:34 -0600

    Add candle

---
+++ src/candle/candle.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+@export var random_delay := true
+@export_range(0, 1, 0.01) var delay_max_sec := 0.5
+
+@onready var _animation_player: AnimationPlayer = $Flame/AnimationPlayer
+
+
+func _ready() -> void:
+	await get_tree().create_timer(randf_range(0, delay_max_sec)).timeout
+	_animation_player.play("candle")
