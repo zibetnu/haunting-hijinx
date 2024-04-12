bf519fbb5abafe2631a9aebd088cffa00e8a91db
zibetnu <9at25jnr3@mozmail.com>
2024-04-12 10:13:55 -0600

    Add app ID to use when initializing Steam

--- src/autoload/steam_callbacks.gd
+++ src/autoload/steam_callbacks.gd
@@ -1,8 +1,11 @@
 extends Node
 
 
+const APP_ID_PATH = "application/config/steam_app_id"
+
+
 func _ready() -> void:
-	Steam.steamInit()
+	Steam.steamInit(true, ProjectSettings.get_setting(APP_ID_PATH, 0))
 	set_process(Steam.isSteamRunning())
 
 
