689e6b9c600d7a7c7e1df467c4eae62ffb0e55c2
zibetnu <9at25jnr3@mozmail.com>
2024-04-24 09:12:59 -0600

    Add relay initializer to lobby

--- src/lobby/lobby_location_setter/lobby_location_setter.gd
+++ src/lobby/lobby_location_setter/lobby_location_setter.gd
@@ -8,16 +8,6 @@ const PLACEHOLDER_LOCATION := [0]
 const PLACEHOLDER_LOBBY_ID := -1
 
 
-func _ready() -> void:
-	Steam.relay_network_status.connect(_on_relay_network_status)
-	match Steam.getRelayNetworkStatus():
-		Steam.NETWORKING_AVAILABILITY_NEVER_TRIED:
-			Steam.initRelayNetworkAccess()
-
-		Steam.NETWORKING_AVAILABILITY_CURRENT:
-			set_lobby_location()
-
-
 func set_lobby_location() -> void:
 	var lobby_id := _get_autoload_lobby_id()
 	if Steam.getSteamID() != Steam.getLobbyOwner(lobby_id):
@@ -43,14 +33,3 @@ func _get_autoload_lobby_id() -> int:
 		return PLACEHOLDER_LOBBY_ID
 
 	return int(lobby_id)
-
-
-func _on_relay_network_status(
-		available: int,
-		_ping_measurement: int,
-		_available_config: int,
-		_available_relay: int,
-		_debug_message: String
-) -> void:
-	if available == Steam.NETWORKING_AVAILABILITY_CURRENT:
-		set_lobby_location()
