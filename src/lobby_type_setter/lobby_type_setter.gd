2067e932d4eb474223bbc85674c263e558dc8092
zibetnu <9at25jnr3@mozmail.com>
2024-05-15 09:38:09 -0600

    Add lobby type setter

---
+++ src/lobby_type_setter/lobby_type_setter.gd
@@ -0,0 +1,69 @@
+# TODO: refactor separate lobby setter scenes to be one scene.
+extends Node
+
+
+signal lobby_type_on_ready(lobby_type: Steam.LobbyType)
+
+const AUTOLOAD_PATH = ^"/root/PeerData"
+const LOBBY_ID_PROPERTY = &"lobby_id"
+const UNKNOWN_LOBBY_ID = -1
+
+const TYPE_KEY = "type"
+const TYPES_TO_STRINGS = {
+	Steam.LOBBY_TYPE_PRIVATE: "Private",
+	Steam.LOBBY_TYPE_FRIENDS_ONLY: "Friends Only",
+	Steam.LOBBY_TYPE_PUBLIC: "Public",
+	Steam.LOBBY_TYPE_INVISIBLE: "Invisible",
+	Steam.LOBBY_TYPE_PRIVATE_UNIQUE: "Private Unique",
+}
+
+const DEFAULT_LOBBY_TYPE = Steam.LOBBY_TYPE_PRIVATE
+
+@onready var lobby_id: int = _get_autoload_lobby_id()
+
+
+func _ready() -> void:
+	lobby_type_on_ready.emit(get_lobby_type())
+
+
+func get_lobby_type_string() -> String:
+	return Steam.getLobbyData(lobby_id, TYPE_KEY)
+
+
+func get_lobby_type() -> Steam.LobbyType:
+	var lobby_type_string := get_lobby_type_string()
+	var type_strings: Array[String] = []
+	type_strings.assign(TYPES_TO_STRINGS.values())
+
+	if not lobby_type_string in type_strings:
+		set_lobby_type(DEFAULT_LOBBY_TYPE)
+		return DEFAULT_LOBBY_TYPE
+
+	return TYPES_TO_STRINGS.keys()[type_strings.find(lobby_type_string)]
+
+
+func set_lobby_type(value: Steam.LobbyType) -> void:
+	if Steam.getSteamID() != Steam.getLobbyOwner(lobby_id):
+		return
+
+	if value not in TYPES_TO_STRINGS.keys():
+		return
+
+	Steam.setLobbyType(lobby_id, value)
+	Steam.setLobbyData(
+		lobby_id,
+		TYPE_KEY,
+		TYPES_TO_STRINGS.get(value) as String
+	)
+
+
+func _get_autoload_lobby_id() -> int:
+	var autoload := get_node_or_null(AUTOLOAD_PATH)
+	if autoload == null:
+		return UNKNOWN_LOBBY_ID
+
+	var value: Variant = autoload.get(LOBBY_ID_PROPERTY)
+	if not value is int:
+		return UNKNOWN_LOBBY_ID
+
+	return value as int
