ee6e39bb6cd548ac18336eec2cc2f997b4ebdf8b
zibetnu <zibetnu@pm.me>
2024-06-15 19:54:45 -0600

    Add HeartbeatRumble

---
+++ src/heartbeat_rumble/heartbeat_rumble.gd
@@ -0,0 +1,59 @@
+extends Node
+## Mimics a heartbeat using gamepad vibration.
+##
+## Useful as a way of alerting a player without using visuals.
+
+
+## Each value is the number of heartbeats per minute associated with the key.
+enum HeartRate {
+	NORMAL = 20,
+	NERVOUS = 90,
+	TERRIFIED = 180,
+}
+
+const DEVICE_INDEX = 0
+const RUMBLE_DURATION = 0.2
+const STRONG_MAGNITUDE = 0.0
+const WEAK_MAGNITUDE = 0.1
+
+const _MINUTE = 60
+
+@export var enabled := true: set = set_enabled
+
+## The number of heartbeats per minute.
+@export var heart_rate: HeartRate = HeartRate.NORMAL
+
+var _time_elapsed := 0.0
+
+
+func _process(delta: float) -> void:
+	_time_elapsed += delta
+	if _time_elapsed < _MINUTE / float(heart_rate):
+		return
+
+	_time_elapsed = 0.0
+	_vibrate()
+
+
+func set_enabled(value: bool) -> void:
+	enabled = value
+
+
+## Sets [enum heart_rate] to the [enum HeartRate] value pointed to by
+## [param index] as if [enum HeartRate] was an [Array]. Has no effect if
+## [param index] is out of range.
+func set_heart_rate_from_index(index: int) -> void:
+	if index < 0:
+		return
+
+	if index >= HeartRate.size():
+		return
+
+	heart_rate = HeartRate[HeartRate.keys()[index]]
+
+
+func _vibrate() -> void:
+	# Setting duration in start_joy_vibration doesn't seem to work properly.
+	Input.start_joy_vibration(DEVICE_INDEX, WEAK_MAGNITUDE, STRONG_MAGNITUDE)
+	await get_tree().create_timer(RUMBLE_DURATION).timeout
+	Input.stop_joy_vibration(DEVICE_INDEX)
