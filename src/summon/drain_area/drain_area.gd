df9680bca4fa10c9d224102615b5cc737d86960d
zibetnu <zibetnu@pm.me>
2024-06-29 11:28:26 -0600

    Add drain area activation method and signal

--- src/summon/drain_area/drain_area.gd
+++ src/summon/drain_area/drain_area.gd
@@ -1,10 +1,25 @@
 extends Area2D
 
 
+signal activated
+
 @export var damage_source: DamageSource
 
 var _active := false
 var _affected_collision_objects: Array[CollisionObject2D] = []
+
+
+func activate() -> void:
+	if not multiplayer.is_server():
+		return
+
+	_active = true
+	activated.emit()
+	_damage_affected_collision_objects()
+	$Foreground.material.set_shader_parameter("amplitude", 2)
+	$Foreground.material.set_shader_parameter("frequency", 4)
+	$Foreground.material.set_shader_parameter("ripple_rate", 12)
+	$ActiveTimer.start()
 
 
 func _damage_affected_collision_objects() -> void:
@@ -31,17 +46,3 @@ func _on_entered(collision_object: CollisionObject2D) -> void:
 
 func _on_exited(collision_object: CollisionObject2D) -> void:
 	_affected_collision_objects.erase(collision_object)
-
-
-func _on_idle_timer_timeout() -> void:
-	_active = true
-	_damage_affected_collision_objects()
-	$Foreground.material.set_shader_parameter("amplitude", 2)
-	$Foreground.material.set_shader_parameter("frequency", 4)
-	$Foreground.material.set_shader_parameter("ripple_rate", 12)
-	$ActiveTimer.start()
-
-
-func _on_active_timer_timeout() -> void:
-	if multiplayer.is_server():
-		queue_free()
