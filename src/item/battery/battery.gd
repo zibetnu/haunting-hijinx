57b1a9138f1bae6808042bf41c9146b91a9472e6
zibetnu <zibetnu@pm.me>
2024-06-30 14:46:23 -0600

    Clear battery inheritance

--- src/item/battery/battery.gd
+++ src/item/battery/battery.gd
@@ -1,4 +1,4 @@
-extends Item
+extends Area2D
 
 
 @export var weak_to: DamageSource.Type
@@ -36,3 +36,22 @@ func _acquirer_allowed(acquirer: Variant) -> bool:
 
 func _on_acquired(acquirer: Variant) -> void:
 	acquirer.battery_percentage = 1
+
+
+func _on_entered(acquirer: Variant) -> void:
+	if not _acquirer_allowed(acquirer):
+		return
+
+	if not multiplayer.is_server():
+		return
+
+	_on_acquired(acquirer)
+	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	_on_entered(area)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	_on_entered(body)
