10e7f2f464b667669fa627be98ce63208b1a53f3
zibetnu <zibetnu@pm.me>
2024-06-30 16:20:40 -0600

    Separate battery behavior and display

---
+++ src/battery/behavior/behavior.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+
+signal acquired
+signal disabled
+
+const ACQUIRER_PROPERTY = &"battery_percentage"
+const ACQUIRER_VALUE = 1.0
+
+@export var weak_to: DamageSource.Type
+
+@onready var _collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+
+func disable() -> void:
+	_collision_shape_2d.disabled = true
+	disabled.emit()
+
+
+func take_damage(source: DamageSource) -> void:
+	if source.damage_type != weak_to:
+		return
+
+	disable()
+
+
+func _on_entered(acquirer: Object) -> void:
+	if not ACQUIRER_PROPERTY in acquirer:
+		return
+
+	acquirer.set(ACQUIRER_PROPERTY, ACQUIRER_VALUE)
+	acquired.emit()
