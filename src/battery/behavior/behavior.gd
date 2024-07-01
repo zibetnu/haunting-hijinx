0c14eecad7a17fd1ad80cbff2d4eda913fefd55e
zibetnu <zibetnu@pm.me>
2024-07-01 17:07:23 -0600

    Delete damage method from battery behavior

--- src/battery/behavior/behavior.gd
+++ src/battery/behavior/behavior.gd
@@ -7,8 +7,6 @@ signal destroyed
 const ACQUIRER_PROPERTY = &"battery_percentage"
 const ACQUIRER_VALUE = 1.0
 
-@export var weak_to: DamageSource.Type
-
 
 func acquire(acquirer: Object) -> void:
 	if not ACQUIRER_PROPERTY in acquirer:
@@ -22,10 +20,3 @@ func acquire(acquirer: Object) -> void:
 func destroy() -> void:
 	destroyed.emit()
 	queue_free()
-
-
-func take_damage(source: DamageSource) -> void:
-	if source.damage_type != weak_to:
-		return
-
-	destroy()
