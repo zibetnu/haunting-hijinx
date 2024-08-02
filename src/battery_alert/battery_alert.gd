33bfe35d45befbe77f1b767fc6763e5c03e53e76
zibetnu <zibetnu@pm.me>
2024-08-02 15:22:35 -0600

    Fix BatteryAlert not syncing properly

--- src/battery_alert/battery_alert.gd
+++ src/battery_alert/battery_alert.gd
@@ -2,15 +2,19 @@ extends Control
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var _timer: Timer = $Timer
 
 
 func notify_dead() -> void:
 	animation_player.play("battery_dead")
+	_timer.start()
 
 
 func notify_low() -> void:
 	animation_player.play("battery_low")
+	_timer.start()
 
 
 func notify_none() -> void:
 	animation_player.play("RESET")
+	_timer.stop()
