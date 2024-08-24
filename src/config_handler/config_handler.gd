cb89fb66dc8829178955017e1ab35dfd86b8c8ae
zibetnu <zibetnu@pm.me>
2024-08-24 14:54:55 -0600

    Fix ConfigHandler erasing all other values on save

--- src/config_handler/config_handler.gd
+++ src/config_handler/config_handler.gd
@@ -15,12 +15,7 @@ const PATH_SETTING_NAME = "application/config/config_path"
 
 
 func load_value() -> void:
-	var config_file := ConfigFile.new()
-	var error: Error = config_file.load(_path)
-	if error != OK:
-		load_failed.emit()
-		return
-
+	var config_file: ConfigFile = _get_existing_config_file()
 	if not config_file.has_section_key(section, key):
 		load_failed.emit()
 		return
@@ -29,7 +24,7 @@ func load_value() -> void:
 
 
 func save_value(value: Variant) -> void:
-	var config_file := ConfigFile.new()
+	var config_file: ConfigFile = _get_existing_config_file()
 	config_file.set_value(section, key, value)
 	var error: Error = config_file.save(_path)
 	if error != OK:
@@ -37,3 +32,9 @@ func save_value(value: Variant) -> void:
 		return
 
 	saved.emit()
+
+
+func _get_existing_config_file() -> ConfigFile:
+	var config_file := ConfigFile.new()
+	config_file.load(_path)
+	return config_file
