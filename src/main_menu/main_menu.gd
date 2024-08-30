5a7dfec31f4a801f9e78046ceb0ed3e4e5e58900
zibetnu <zibetnu@pm.me>
2024-08-30 12:21:23 -0600

    Refactor main menu

--- src/main_menu/main_menu.gd
+++ src/main_menu/main_menu.gd
@@ -1,13 +1,6 @@
 extends Control
 
 
-@export var first_button: Button
-
-
-func _ready() -> void:
-	first_button.grab_focus()
-
-
 func _on_browse_lobbies_pressed() -> void:
 	SceneChanger.change_to_lobby_browser()
 
