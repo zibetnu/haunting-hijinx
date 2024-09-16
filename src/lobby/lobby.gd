e5db0fca5cd157bddfb246c908b8b3141c7e2436
zibetnu <zibetnu@pm.me>
2024-09-16 12:34:25 -0600

    Refactor Lobby

--- src/lobby/lobby.gd
+++ src/lobby/lobby.gd
@@ -3,17 +3,22 @@ extends Control
 
 signal close_connection_requested
 
-const AUTOLOAD_LOBBY_PROPERTY := &"lobby_id"
-const AUTOLOAD_PATH := ^"/root/PeerData"
+const CLOSE_CONNECTION_ACTION = &"ui_cancel"
 const MIN_PARTICIPANTS = 1
 const TOGGLE_BUTTON_PATH = ^"%ParticipationToggle"
 
 @export var level: PackedScene
 @export var player_card: PackedScene
 
+@onready var active_cards: VBoxContainer = %ActiveCards
+@onready var ghost_selector: OptionButton = %GhostSelector
+@onready var host_menu: GridContainer = %HostMenu
+@onready var spectate_cards: VBoxContainer = %SpectateCards
+@onready var start_button: Button = %StartButton
 
-func _ready():
-	%HostMenu.visible = multiplayer.is_server()
+
+func _ready() -> void:
+	host_menu.visible = multiplayer.is_server()
 
 	# Only the server needs to spawn the players.
 	if not multiplayer.is_server():
@@ -25,49 +30,55 @@ func _ready():
 	PeerData.peer_name_changed.connect(_on_peer_name_changed)
 	PeerData.peer_participation_changed.connect(_on_peer_participation_changed)
 
-	for peer_id in PeerData.participants:
+	for peer_id: int in PeerData.participants:
 		add_participant(peer_id)
 
-	for peer_id in PeerData.spectators:
+	for peer_id: int in PeerData.spectators:
 		add_spectator(peer_id)
 
 	_on_ghost_peer_changed(PeerData.ghost_peer)
-	%GhostSelector.grab_focus()
+	ghost_selector.grab_focus()
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed(CLOSE_CONNECTION_ACTION):
 		close_connection_requested.emit()
 
 
 func add_participant(id: int) -> void:
-	var card := _instantiate_card(id)
-	%ActiveCards.add_child(card, true)
-	%GhostSelector.add_item(PeerData.peer_names[card.input_authority], card.input_authority)
+	var card: Node = _instantiate_card(id)
+	active_cards.add_child(card, true)
+	ghost_selector.add_item(
+			str(PeerData.peer_names[card.input_authority]),
+			card.input_authority
+	)
 	_on_peer_participation_changed(id)
 
 
 func add_spectator(id: int) -> void:
-	var card := _instantiate_card(id)
-	%SpectateCards.add_child(card, true)
-	%GhostSelector.add_item(PeerData.peer_names[card.input_authority], card.input_authority)
+	var card: Node = _instantiate_card(id)
+	spectate_cards.add_child(card, true)
+	ghost_selector.add_item(
+			str(PeerData.peer_names[card.input_authority]),
+			card.input_authority
+	)
 	_on_peer_participation_changed(id)
 
 
-func _get_autoload_lobby_id() -> Variant:
-	var autoload := get_node_or_null(AUTOLOAD_PATH)
-	if autoload:
-		return autoload.get(AUTOLOAD_LOBBY_PROPERTY)
-
-	return null
-
-
 func _instantiate_card(id: int) -> Node:
-	var card = player_card.instantiate()
+	var card: Node = player_card.instantiate()
 	card.name += str(id)
 	card.input_authority = id
-	card.player_name_changed.connect(func(value): PeerData.set_peer_name(id, value))
+	card.player_name_changed.connect(
+			func(value: String) -> void: PeerData.set_peer_name(id, value)
+	)
 	return card
+
+
+func _multiplayer_reparent(child: Node, new_parent: Node) -> void:
+		child.get_parent().remove_child(child)
+		child.request_ready()
+		new_parent.add_child(child, true)
 
 
 func _on_connection_closed() -> void:
@@ -75,7 +86,7 @@ func _on_connection_closed() -> void:
 
 
 func _on_ghost_peer_changed(id: int) -> void:
-	%GhostSelector.select(%GhostSelector.get_item_index(id))
+	ghost_selector.select(ghost_selector.get_item_index(id))
 
 
 func _on_peer_connected(id: int) -> void:
@@ -87,55 +98,62 @@ func _on_peer_connected(id: int) -> void:
 
 
 func _on_peer_disconnected(id: int) -> void:
-	for card in %ActiveCards.get_children() + %SpectateCards.get_children():
+	for card in active_cards.get_children() + spectate_cards.get_children():
 		if card.input_authority == id:
 			card.queue_free()
 
-	if %GhostSelector.get_item_index(id) != -1:
-		%GhostSelector.remove_item(%GhostSelector.get_item_index(id))
-		%GhostSelector.select(%GhostSelector.get_selectable_item())
+	if ghost_selector.get_item_index(id) != -1:
+		ghost_selector.remove_item(ghost_selector.get_item_index(id))
+		ghost_selector.select(ghost_selector.get_selectable_item())
 
 
 func _on_peer_name_changed(id: int) -> void:
-	var index: int = %GhostSelector.get_item_index(id)
+	var index: int = ghost_selector.get_item_index(id)
 	if index == -1:
 		return
 
-	%GhostSelector.set_item_text(index, PeerData.peer_names[id])
+	ghost_selector.set_item_text(index, str(PeerData.peer_names[id]))
 
 
 func _on_peer_participation_changed(id: int) -> void:
-	var disabled := not (id in PeerData.participants)
-	%GhostSelector.set_item_disabled(%GhostSelector.get_item_index(id), disabled)
+	if not multiplayer.is_server():
+		return
+
+	var is_participant: bool = id in PeerData.participants
+	var is_spectator: bool = id in PeerData.spectators
+	ghost_selector.set_item_disabled(
+			ghost_selector.get_item_index(id), is_spectator
+	)
 
 	# In cases where the selected id may be disabled or
 	# no id is selected, select the first available id.
-	if disabled or %GhostSelector.get_selected_id() == -1:
-		%GhostSelector.select(%GhostSelector.get_selectable_item())
+	if is_spectator or ghost_selector.get_selected_id() == -1:
+		ghost_selector.select(ghost_selector.get_selectable_item())
 
-	%StartButton.disabled = PeerData.participants.size() < MIN_PARTICIPANTS
-	for card in %ActiveCards.get_children() + %SpectateCards.get_children():
+	start_button.disabled = PeerData.participants.size() < MIN_PARTICIPANTS
+	for card in active_cards.get_children() + spectate_cards.get_children():
 		if card.input_authority != id:
 			continue
 
-		var player_name: String = card.player_name
-		var input_authority: int = card.input_authority
-		card.queue_free()
-		var new_card = _instantiate_card(input_authority)
-		new_card.player_name = player_name
+		var parent: Node = card.get_parent()
+		if is_participant and parent == active_cards:
+			continue
 
-		if id in PeerData.participants:
-			%ActiveCards.add_child(new_card, true)
+		if is_spectator and parent == spectate_cards:
+			continue
 
-		elif id in PeerData.spectators:
-			%SpectateCards.add_child(new_card, true)
+		if is_participant:
+			_multiplayer_reparent(card, active_cards)
 
-		# Grab focus again after it was lost by freeing the prior card.
-		var toggle_button := new_card.get_node_or_null(TOGGLE_BUTTON_PATH) as Button
+		if is_spectator:
+			_multiplayer_reparent(card, spectate_cards)
+
+		# Grab focus again after it was lost by reparenting the card.
+		var toggle_button := card.get_node_or_null(TOGGLE_BUTTON_PATH) as Button
 		if toggle_button != null:
 			toggle_button.grab_focus.call_deferred()
 
 
 func _on_start_button_pressed() -> void:
-	PeerData.ghost_peer = %GhostSelector.get_selected_id()
+	PeerData.ghost_peer = ghost_selector.get_selected_id()
 	SceneChanger.change_scene_to_packed(level)
