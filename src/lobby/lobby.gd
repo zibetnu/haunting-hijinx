4130e6209b2263a221d601277f18cb49c17d0b9d
zibetnu <zibetnu@pm.me>
2024-09-18 22:19:17 -0600

    Revise Lobby and PlayerCard

--- src/lobby/lobby.gd
+++ src/lobby/lobby.gd
@@ -5,15 +5,15 @@ signal close_connection_requested
 
 const CLOSE_CONNECTION_ACTION = &"ui_cancel"
 const MIN_PARTICIPANTS = 1
+const PLAYER_TYPE_METHOD = &"set_player_type"
+const PLAYER_TYPE_SIGNAL = &"player_type_changed"
 const TOGGLE_BUTTON_PATH = ^"%ParticipationToggle"
 
 @export var level: PackedScene
 @export var player_card: PackedScene
 
-@onready var active_cards: VBoxContainer = %ActiveCards
-@onready var ghost_selector: OptionButton = %GhostSelector
+@onready var cards: VBoxContainer = %Cards
 @onready var host_menu: GridContainer = %HostMenu
-@onready var spectate_cards: VBoxContainer = %SpectateCards
 @onready var start_button: Button = %StartButton
 
 
@@ -26,18 +26,9 @@ func _ready() -> void:
 
 	multiplayer.peer_connected.connect(_on_peer_connected)
 	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
-	PeerData.ghost_peer_changed.connect(_on_ghost_peer_changed)
-	PeerData.peer_name_changed.connect(_on_peer_name_changed)
 	PeerData.peer_participation_changed.connect(_on_peer_participation_changed)
-
-	for peer_id: int in PeerData.participants:
-		add_participant(peer_id)
-
-	for peer_id: int in PeerData.spectators:
-		add_spectator(peer_id)
-
-	_on_ghost_peer_changed(PeerData.ghost_peer)
-	ghost_selector.grab_focus()
+	for peer_id: int in PeerData.participants + PeerData.spectators:
+		add_card(peer_id)
 
 
 func _input(event: InputEvent) -> void:
@@ -45,115 +36,83 @@ func _input(event: InputEvent) -> void:
 		close_connection_requested.emit()
 
 
-func add_participant(id: int) -> void:
-	var card: Node = _instantiate_card(id)
-	active_cards.add_child(card, true)
-	ghost_selector.add_item(
-			str(PeerData.peer_names[card.input_authority]),
-			card.input_authority
-	)
-	_on_peer_participation_changed(id)
-
-
-func add_spectator(id: int) -> void:
-	var card: Node = _instantiate_card(id)
-	spectate_cards.add_child(card, true)
-	ghost_selector.add_item(
-			str(PeerData.peer_names[card.input_authority]),
-			card.input_authority
-	)
-	_on_peer_participation_changed(id)
-
-
-func _instantiate_card(id: int) -> Node:
+func add_card(id: int) -> void:
 	var card: Node = player_card.instantiate()
 	card.name += str(id)
 	card.input_authority = id
 	card.player_name_changed.connect(
 			func(value: String) -> void: PeerData.set_peer_name(id, value)
 	)
-	return card
+	if card.has_signal(PLAYER_TYPE_SIGNAL):
+		card.connect(
+				PLAYER_TYPE_SIGNAL,
+				func(value: int)-> void: TypeAdapter.set_peer_type(id, value)
+		)
+	cards.add_child(card, true)
+	if card.has_method(PLAYER_TYPE_METHOD):
+		card.call(PLAYER_TYPE_METHOD, TypeAdapter.get_peer_type(id))
 
-
-func _multiplayer_reparent(child: Node, new_parent: Node) -> void:
-		child.get_parent().remove_child(child)
-		child.request_ready()
-		new_parent.add_child(child, true)
+	_on_peer_participation_changed(id)
 
 
 func _on_connection_closed() -> void:
 	SceneChanger.change_to_lobby_browser()
 
 
-func _on_ghost_peer_changed(id: int) -> void:
-	ghost_selector.select(ghost_selector.get_item_index(id))
-
-
 func _on_peer_connected(id: int) -> void:
-	if id in PeerData.participants:
-		add_participant(id)
-
-	elif id in PeerData.spectators:
-		add_spectator(id)
+	add_card(id)
 
 
 func _on_peer_disconnected(id: int) -> void:
-	for card in active_cards.get_children() + spectate_cards.get_children():
+	for card in cards.get_children():
 		if card.input_authority == id:
 			card.queue_free()
 
-	if ghost_selector.get_item_index(id) != -1:
-		ghost_selector.remove_item(ghost_selector.get_item_index(id))
-		ghost_selector.select(ghost_selector.get_selectable_item())
 
-
-func _on_peer_name_changed(id: int) -> void:
-	var index: int = ghost_selector.get_item_index(id)
-	if index == -1:
-		return
-
-	ghost_selector.set_item_text(index, str(PeerData.peer_names[id]))
-
-
-func _on_peer_participation_changed(id: int) -> void:
+func _on_peer_participation_changed(_id: int) -> void:
 	if not multiplayer.is_server():
 		return
 
-	var is_participant: bool = id in PeerData.participants
-	var is_spectator: bool = id in PeerData.spectators
-	ghost_selector.set_item_disabled(
-			ghost_selector.get_item_index(id), is_spectator
-	)
-
-	# In cases where the selected id may be disabled or
-	# no id is selected, select the first available id.
-	if is_spectator or ghost_selector.get_selected_id() == -1:
-		ghost_selector.select(ghost_selector.get_selectable_item())
-
 	start_button.disabled = PeerData.participants.size() < MIN_PARTICIPANTS
-	for card in active_cards.get_children() + spectate_cards.get_children():
-		if card.input_authority != id:
-			continue
-
-		var parent: Node = card.get_parent()
-		if is_participant and parent == active_cards:
-			continue
-
-		if is_spectator and parent == spectate_cards:
-			continue
-
-		if is_participant:
-			_multiplayer_reparent(card, active_cards)
-
-		if is_spectator:
-			_multiplayer_reparent(card, spectate_cards)
-
-		# Grab focus again after it was lost by reparenting the card.
-		var toggle_button := card.get_node_or_null(TOGGLE_BUTTON_PATH) as Button
-		if toggle_button != null:
-			toggle_button.grab_focus.call_deferred()
 
 
 func _on_start_button_pressed() -> void:
-	PeerData.ghost_peer = ghost_selector.get_selected_id()
 	SceneChanger.change_scene_to_packed(level)
+
+
+class TypeAdapter:
+	enum PeerType {
+		HUNTER = 0,
+		GHOST = 1,
+		SPECTATOR = 2,
+	}
+
+
+	static func get_peer_type(id: int) -> PeerType:
+		if id in PeerData.spectators:
+			return PeerType.SPECTATOR
+
+		if id == PeerData.ghost_peer:
+			return PeerType.GHOST
+
+		return PeerType.HUNTER
+
+
+	static func set_peer_type(id: int, type: PeerType) -> void:
+		match type:
+			PeerType.HUNTER:
+				if id in PeerData.spectators:
+					PeerData.toggle_participation(id)
+
+				if id == PeerData.ghost_peer:
+					PeerData.ghost_peer = -1
+
+			PeerType.GHOST:
+				if id in PeerData.spectators:
+					PeerData.toggle_participation(id)
+
+				PeerData.ghost_peer = id
+
+			PeerType.SPECTATOR:
+				if id in PeerData.participants:
+					PeerData.toggle_participation(id)
--- src/lobby/player_card/player_card.gd
+++ src/lobby/player_card/player_card.gd
@@ -2,40 +2,103 @@ extends PanelContainer
 
 
 signal player_name_changed(value: String)
+signal player_type_changed(value: PlayerType)
 
 const AUTOLOAD_NAMES_PROPERTY := &"peer_names"
 const AUTOLOAD_PATH := ^"/root/PeerData"
+const CARD_GROUP = &"player_cards"
+const GROUP_METHOD = &"auto_set_player_type"
+const MAX_GHOSTS = 1
+const MAX_HUNTERS = 4
+
+enum PlayerType {
+	HUNTER = 0,
+	GHOST = 1,
+	SPECTATOR = 2,
+}
 
 @export var input_authority := 1:
 	set(value):
 		input_authority = value
 		%InputSynchronizer.set_multiplayer_authority(value)
-		%IDLabel.text = str(value)
+
+@export var player_type: PlayerType:
+	set = set_player_type
 
 var player_name: String:
-	set(value):
-		player_name = value
-		player_name_changed.emit(player_name)
-		%NameLabel.text = value
+	set = set_player_name
+
+@onready var cycle_type: Button = %CycleType
+@onready var name_line_edit: LineEdit = %NameLineEdit
 
 
 func _ready() -> void:
-	var participation_signal: Signal = PeerData.peer_participation_changed
-	var participation_receiver: Callable = _on_peer_participation_changed
-	if not participation_signal.is_connected(participation_receiver):
-		participation_signal.connect(participation_receiver)
-
-	set_player_name(str(_get_autoload_peer_name(input_authority)), true)
-	%NameLineEdit.visible = multiplayer.get_unique_id() == input_authority
-	%NameLabel.visible = not %NameLineEdit.visible
-	%ParticipationToggle.visible = multiplayer.is_server()
-	_on_peer_participation_changed(input_authority)
+	cycle_type.disabled = not multiplayer.is_server()
+	name_line_edit.editable = multiplayer.get_unique_id() == input_authority
+	set_player_name(str(_get_autoload_peer_name(input_authority)))
 
 
-func set_player_name(value: String, update_line_edit := false) -> void:
+func auto_set_player_type(caller: Node) -> void:
+	if caller == self:
+		return
+
+	if player_type == PlayerType.SPECTATOR:
+		return
+
+	var cards: Array[Node] = get_tree().get_nodes_in_group(CARD_GROUP)
+	var player_types: Array[PlayerType] = []
+	player_types.assign(
+		cards.map(func(card: Node) -> PlayerType: return card.get("player_type"))
+	)
+
+	var ghosts_full: bool = player_types.count(PlayerType.GHOST) > MAX_GHOSTS
+	var hunters_full: bool = player_types.count(PlayerType.HUNTER) > MAX_HUNTERS
+	if not (ghosts_full or hunters_full):
+		return
+
+	var new_player_type: PlayerType = player_type
+	match player_type:
+		PlayerType.HUNTER:
+			if hunters_full:
+				new_player_type = PlayerType.GHOST
+
+			if ghosts_full:
+				new_player_type = PlayerType.SPECTATOR
+
+		PlayerType.GHOST:
+			if ghosts_full:
+				new_player_type = PlayerType.HUNTER
+
+			if hunters_full:
+				new_player_type = PlayerType.SPECTATOR
+
+	if player_type == new_player_type:
+		return
+
+	set_player_type(new_player_type)
+
+
+func cycle_player_type() -> void:
+	set_player_type((player_type + 1) % PlayerType.size())
+
+
+func set_player_name(value: String) -> void:
 	player_name = value
-	if update_line_edit:
-		%NameLineEdit.text = player_name
+	player_name_changed.emit(player_name)
+	var caret_column: int = name_line_edit.caret_column
+	name_line_edit.text = player_name
+	name_line_edit.caret_column = caret_column
+
+
+func set_player_type(value: PlayerType) -> void:
+	player_type = value
+	player_type_changed.emit(player_type)
+	_call_card_group_sequentially()
+
+
+func _call_card_group_sequentially() -> void:
+	for card: Node in get_tree().get_nodes_in_group(CARD_GROUP):
+		card.call(GROUP_METHOD, self)
 
 
 func _get_autoload_peer_name(id: int) -> Variant:
@@ -52,13 +115,3 @@ func _get_autoload_peer_name(id: int) -> Variant:
 
 func _on_participation_toggle_pressed() -> void:
 	PeerData.toggle_participation(input_authority)
-
-
-func _on_peer_participation_changed(_id: int) -> void:
-	if input_authority in PeerData.participants:
-		%ParticipationToggle.disabled = false
-		%ParticipationToggle.text = ">>"
-
-	else:
-		%ParticipationToggle.disabled = PeerData.participants.size() >= PeerData.MAX_PARTICIPANTS
-		%ParticipationToggle.text = "<<"
