ca67b626b188457d24a51df22b6bbdbdffbff05f
zibetnu <zibetnu@pm.me>
2024-10-17 18:39:00 -0600

    Update KeyboardAndMouse look_vector when mouse visible

--- src/controller/keyboard_and_mouse/keyboard_and_mouse.gd
+++ src/controller/keyboard_and_mouse/keyboard_and_mouse.gd
@@ -1,23 +1,15 @@
 extends Controller
 
 
-var _mouse_active := false
+func _process(_delta: float) -> void:
+	_look_at_mouse_if_visible()
+	if _is_mouse_visible():
+		input_handled.emit()
 
 
-func _unhandled_input(event: InputEvent) -> void:
+func _unhandled_input(_event: InputEvent) -> void:
 	move_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if event is InputEventMouseMotion:
-		_mouse_active = true
-		$Timer.start()
-
-	if _mouse_active:
-		look_vector = Vector2.from_angle(
-				global_position.angle_to_point(get_global_mouse_position())
-		)
-
-	else:
-		look_vector = Vector2.ZERO
-
+	_look_at_mouse_if_visible()
 	button_1 = Input.is_action_pressed("button_1")
 	button_2 = Input.is_action_pressed("button_2")
 
@@ -26,18 +18,24 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func force_handle_input() -> void:
 	move_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if _mouse_active:
-		look_vector = Vector2.from_angle(
-				global_position.angle_to_point(get_global_mouse_position())
-		)
-
-	else:
-		look_vector = Vector2.ZERO
-
+	_look_at_mouse_if_visible()
 	button_1 = Input.is_action_pressed("button_1")
 	button_2 = Input.is_action_pressed("button_2")
 	input_handled.emit()
 
 
-func _on_timer_timeout() -> void:
-	_mouse_active = false
+func _is_mouse_visible() -> bool:
+	return not Input.get_mouse_mode() in [
+			Input.MOUSE_MODE_CONFINED_HIDDEN,
+			Input.MOUSE_MODE_HIDDEN
+	]
+
+
+func _look_at_mouse_if_visible() -> void:
+	if not _is_mouse_visible():
+		look_vector = Vector2.ZERO
+		return
+
+	look_vector = Vector2.from_angle(
+			global_position.angle_to_point(get_global_mouse_position())
+	)
