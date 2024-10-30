c5d3b55b24280b67fce1d197b546099bb80b9a7d
zibetnu <zibetnu@pm.me>
2024-10-30 17:49:56 -0600

    Fix MouseHider not working in sub-windows

--- src/autoload/mouse_hider.gd
+++ src/autoload/mouse_hider.gd
@@ -6,6 +6,7 @@ const INACTIVE_MODE = Input.MOUSE_MODE_HIDDEN
 
 
 func _ready() -> void:
+	get_tree().node_added.connect(_on_node_added)
 	Input.set_mouse_mode(INACTIVE_MODE)
 
 
@@ -15,3 +16,12 @@ func _input(event: InputEvent) -> void:
 
 	elif event is InputEventJoypadButton or event is InputEventJoypadMotion:
 		Input.set_mouse_mode(INACTIVE_MODE)
+
+
+# Ensures that MouseHider works in any sub-windows.
+func _on_node_added(node: Node) -> void:
+	var window := node as Window
+	if window == null:
+		return
+
+	window.window_input.connect(_input)
