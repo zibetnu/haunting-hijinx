54717e20a7583c28b459ef26b4bdea14480f4015
zibetnu <zibetnu@pm.me>
2024-11-14 14:30:35 -0700

    Delete unused controllers

--- src/controller/spam/spam.gd
+++
@@ -1,9 +0,0 @@
-extends Controller
-
-
-func _on_timer_timeout() -> void:
-	move_vector = Vector2(randf_range(-1, 1), randf_range(-1, 1))
-	look_vector = Vector2(randf_range(-1, 1), randf_range(-1, 1))
-	button_1 = not button_1
-	button_2 = not button_2
-	input_handled.emit()
