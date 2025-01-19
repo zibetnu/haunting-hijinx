b5331e93c568a21437d6fb997352799a346ca0b4
zibetnu <zibetnu@pm.me>
2025-01-18 19:36:28 -0700

    Add zoom functionality to CenteredCamera

--- src/centered_camera.gd
+++ src/centered_camera.gd
@@ -13,15 +13,21 @@ func _ready() -> void:
 ## limits appear centered in a window that is larger than those limits.
 func center() -> void:
 	var content_scale_size: Vector2i = _window.content_scale_size
-	var limit_size := Vector2i(
+	var limit_size := Vector2(
 			limit_right - limit_left,
 			limit_bottom - limit_top
 	)
+	limit_size *= zoom
 	var new_offset := Vector2.ZERO
 	if content_scale_size.x > limit_size.x:
-		new_offset.x = float(content_scale_size.x - limit_size.x) / 2
+		new_offset.x = (content_scale_size.x - limit_size.x) / 2 * zoom.x
 
 	if content_scale_size.y > limit_size.y:
-		new_offset.y = float(limit_size.y - content_scale_size.y) / 2
+		new_offset.y = (limit_size.y - content_scale_size.y) / 2 * zoom.y
 
 	offset = new_offset
+
+
+func set_zoom_centered(value: Vector2) -> void:
+	zoom = value
+	center()
