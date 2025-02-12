ee49930914fe1bac6015aa467057fb61b9f9ada1
zibetnu <zibetnu@pm.me>
2025-02-11 19:14:44 -0700

    Add grab animation to GhostCostume

--- src/ghost/costume/ghost_costume.gd
+++ src/ghost/costume/ghost_costume.gd
@@ -1,26 +1,27 @@
 @tool  # Allows editor to see results of setting frame_coord_x and y.
 class_name GhostCostume
-extends Sprite2D
+extends Node2D
 
 const _MIN_FRAME = 0
 const _MIN_ROTATION = 0.0
 
 @export var frame_coord_x: int:
-	get = get_frame_coord_x,
 	set = set_frame_coord_x
 @export var frame_coord_y: int:
-	get = get_frame_coord_y,
 	set = set_frame_coord_y
 
+var _min_hframes: int
+var _total_vframes := 0
+
 @onready var ghost: AnimationPlayer = $Ghost
+@onready var sprites: Array[Sprite2D] = [$Main, $Grab]
 
 
-func get_frame_coord_x() -> int:
-	return frame_coords.x
-
-
-func get_frame_coord_y() -> int:
-	return frame_coords.y
+func _ready() -> void:
+	_min_hframes = sprites[0].hframes
+	for sprite in sprites:
+		_min_hframes = mini(_min_hframes, sprite.hframes)
+		_total_vframes += sprite.vframes
 
 
 func play(value: String) -> void:
@@ -31,16 +32,35 @@ func set_costume_rotation(value: float) -> void:
 	frame_coord_x = roundi(remap(
 			fposmod(value, TAU),
 			_MIN_ROTATION, TAU,
-			_MIN_FRAME, hframes
-	)) % hframes
+			_MIN_FRAME, _min_hframes
+	)) % _min_hframes
 
 
 func set_frame_coord_x(value: int) -> void:
-	frame_coords.x = clamp(value, _MIN_FRAME, hframes - 1)
+	if not is_node_ready():
+		await ready
+
+	frame_coord_x = clamp(value, _MIN_FRAME, _min_hframes - 1)
+	for sprite in sprites:
+		sprite.frame_coords.x = frame_coord_x
 
 
 func set_frame_coord_y(value: int) -> void:
-	frame_coords.y = clamp(value, _MIN_FRAME, vframes - 1)
+	if not is_node_ready():
+		await ready
+
+	frame_coord_y = clamp(value, _MIN_FRAME, _total_vframes - 1)
+	for sprite in sprites:
+		sprite.hide()
+
+	var relative_frame_coord_y: int = frame_coord_y
+	var index := 0
+	while relative_frame_coord_y >= sprites[index].vframes:
+		relative_frame_coord_y -= sprites[index].vframes
+		index += 1
+
+	sprites[index].frame_coords.y = relative_frame_coord_y
+	sprites[index].show()
 
 
 func _get_first_partial_match(
