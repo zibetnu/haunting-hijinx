895bdda5f03c81050fab514519441dfea04eb688
zibetnu <zibetnu@pm.me>
2025-02-21 16:20:25 -0700

    Add GhostCostume rotation lock

--- src/ghost/costume/ghost_costume.gd
+++ src/ghost/costume/ghost_costume.gd
@@ -2,6 +2,8 @@
 class_name GhostCostume
 extends Sprite2D
 
+const SOUTH_FRAME_COORD_X = 2
+
 const _MIN_FRAME = 0
 const _MIN_ROTATION = 0.0
 
@@ -11,6 +13,8 @@ const _MIN_ROTATION = 0.0
 @export var frame_coord_y: int:
 	get = get_frame_coord_y,
 	set = set_frame_coord_y
+@export var lock_rotation := false:
+	set = set_lock_rotation
 
 @onready var ghost: AnimationPlayer = $Ghost
 
@@ -28,6 +32,9 @@ func play(value: String) -> void:
 
 
 func set_costume_rotation(value: float) -> void:
+	if lock_rotation:
+		return
+
 	frame_coord_x = roundi(remap(
 			fposmod(value, TAU),
 			_MIN_ROTATION, TAU,
@@ -41,6 +48,13 @@ func set_frame_coord_x(value: int) -> void:
 
 func set_frame_coord_y(value: int) -> void:
 	frame_coords.y = clamp(value, _MIN_FRAME, vframes - 1)
+
+
+func set_lock_rotation(value: bool) -> void:
+	var was_locked: bool = lock_rotation
+	lock_rotation = value
+	if was_locked and not lock_rotation:
+		frame_coord_x = SOUTH_FRAME_COORD_X
 
 
 func _get_first_partial_match(
