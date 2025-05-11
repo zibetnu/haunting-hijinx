d44b37dc6e0420bb284c005a652ac5ac628febef
zibetnu <zibetnu@pm.me>
2025-05-10 18:54:49 -0600

    Refacor VSync option setup

--- src/config_menu/config_menu.gd
+++ src/config_menu/config_menu.gd
@@ -8,17 +8,21 @@ var vsync_config_handler := ConfigHandler.new()
 
 
 func _ready() -> void:
-	vsync_option.selected = DisplayServer.window_get_vsync_mode()
-	vsync_config_handler.section = "display"
-	vsync_config_handler.key = "vsync"
-	vsync_config_handler.loaded.connect(vsync_option.select_id)
-	vsync_config_handler.load_value()
+	init_vsync_option()
 
 
 func _notification(what: int) -> void:
 	match what:
 		NOTIFICATION_PREDELETE:
 			vsync_config_handler.queue_free()
+
+
+func init_vsync_option() -> void:
+	vsync_option.selected = DisplayServer.window_get_vsync_mode()
+	vsync_config_handler.section = "display"
+	vsync_config_handler.key = "vsync"
+	vsync_config_handler.loaded.connect(vsync_option.select_id)
+	vsync_config_handler.load_value()
 
 
 func _on_vsync_option_id_selected(mode: DisplayServer.VSyncMode) -> void:
