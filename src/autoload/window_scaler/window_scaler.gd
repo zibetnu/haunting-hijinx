b6e7dd77b48f76d39eca014657e28ca57af83763
zibetnu <zibetnu@pm.me>
2025-05-10 18:55:47 -0600

    Simplify window scaler

--- src/autoload/window_scaler/window_scaler.gd
+++ src/autoload/window_scaler/window_scaler.gd
@@ -6,14 +6,10 @@ signal scale_multiplier_changed(scale_multiplier: int)
 
 const MIN_SCALE_MULTIPLIER = 1
 
-@export var auto_set_limits := true:
-	set = set_auto_set_limits
 @export var min_value: int = MIN_SCALE_MULTIPLIER:
 	set = set_min_value
 @export var max_value: int = MIN_SCALE_MULTIPLIER:
 	set = set_max_value
-@export var auto_scale := true:
-	set = set_auto_scale
 @export var scale_multiplier: int = MIN_SCALE_MULTIPLIER:
 	set = set_scale_multiplier
 
@@ -27,13 +23,30 @@ var base_size := Vector2i(
 
 func _ready() -> void:
 	add_window_script()
-	_on_window_size_changed()
+	init_min_value()
+	init_max_value()
+	refresh_content_scale_size()
+
+
+func init_min_value() -> void:
+	const BASE_DPI = 100
+	@warning_ignore("integer_division")  # Decimal part should be discarded.
+	min_value = DisplayServer.screen_get_dpi() / BASE_DPI
+
+
+func init_max_value() -> void:
+	var screen_size: Vector2i = DisplayServer.screen_get_size()
+	@warning_ignore("integer_division")  # Decimal part should be discarded.
+	max_value = mini(screen_size.x / base_size.x, screen_size.y / base_size.y)
 
 
 func add_window_script() -> void:
 	window.set_script(WindowSignals)
 	var window_signals := window as WindowSignals
-	window_signals.window_size_changed.connect(_on_window_size_changed)
+	window_signals.window_size_changed.connect(
+			refresh_content_scale_size,
+			CONNECT_DEFERRED
+	)
 
 
 func refresh_content_scale_size() -> void:
@@ -45,16 +58,9 @@ func refresh_content_scale_size() -> void:
 	window.content_scale_size = new_content_scale_size
 
 
-func set_auto_set_limits(value: bool) -> void:
-	auto_set_limits = value
-	_update_min_value()
-	_update_max_value()
-
-
 func set_min_value(value: int) -> void:
 	min_value = maxi(MIN_SCALE_MULTIPLIER, value)
 	min_value_changed.emit(min_value)
-	window.min_size = base_size * min_value
 
 
 func set_max_value(value: int) -> void:
@@ -62,42 +68,11 @@ func set_max_value(value: int) -> void:
 	max_value_changed.emit(max_value)
 
 
-func set_auto_scale(value: bool) -> void:
-	auto_scale = value
-	_update_scale_multiplier()
-
-
 func set_scale_multiplier(value: int) -> void:
 	scale_multiplier = clampi(value, min_value, max_value)
 	scale_multiplier_changed.emit(scale_multiplier)
+	window.min_size = base_size * scale_multiplier
 	refresh_content_scale_size.call_deferred()
-
-
-func _on_window_size_changed() -> void:
-	if auto_set_limits:
-		_update_min_value()
-		_update_max_value()
-
-	if auto_scale:
-		_update_scale_multiplier()
-
-	else:
-		refresh_content_scale_size.call_deferred()
-
-
-func _update_min_value() -> void:
-	const BASE_DPI = 100
-	@warning_ignore("integer_division")  # Decimal part should be discarded.
-	min_value = DisplayServer.screen_get_dpi() / BASE_DPI
-
-
-func _update_max_value() -> void:
-	@warning_ignore("integer_division")  # Decimal part should be discarded.
-	max_value = mini(window.size.x / base_size.x, window.size.y / base_size.y)
-
-
-func _update_scale_multiplier() -> void:
-	set_scale_multiplier(max_value)
 
 
 class WindowSignals:
