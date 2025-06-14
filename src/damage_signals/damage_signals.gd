f53c8cab2d94e5256199a620223305f87aa13942
zibetnu <zibetnu@pm.me>
2025-06-14 16:01:48 -0600

    Replace weak to node

--- src/damage_signals/damage_signals.gd
+++ src/damage_signals/damage_signals.gd
@@ -1,9 +1,16 @@
 extends Area2D
 
+signal damaged_by_amount(damage_amount: int)
 signal damaged_by(source: DamageSource)
 signal damage_taken
 
+@export var weak_to: DamageSource.Type
+
 
 func take_damage(source: DamageSource) -> void:
+	if source.damage_type != weak_to:
+		return
+
 	damaged_by.emit(source)
+	damaged_by_amount.emit(source.damage_amount)
 	damage_taken.emit()
