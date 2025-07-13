0da5d3712991904126d8a8a07bfe02d81c60f8db
zibetnu <zibetnu@pm.me>
2025-07-12 23:03:18 -0600

    Add summon bar tests

---
+++ test/summon_bar_test/summon_bar_test.gd
@@ -0,0 +1,53 @@
+class_name SummonBarTest
+extends GdUnitTestSuite
+
+const START = "start"
+const STOP = "stop"
+
+var _runner: GdUnitSceneRunner
+var _bar: TextureProgressBar
+var _highlight: AnimatedSprite2D
+var _get_animation_speed: Callable
+
+
+func before_test() -> void:
+	_runner = scene_runner("uid://c2p8hmv11uyfp")
+	_bar = _runner.get_property("bar")
+	_highlight = _runner.get_property("highlight")
+	_get_animation_speed = _highlight.sprite_frames.get_animation_speed.bind(
+			&"partial"
+	)
+
+
+func test_start() -> void:
+	_runner.invoke(START, 0.1)
+	await _runner.simulate_frames(10)
+	assert_float(_bar.value).is_greater(0.0)
+	assert_float(_get_animation_speed.call()).is_greater(0.0)
+
+
+func test_stop() -> void:
+	_runner.invoke(START, 0.1)
+	await _runner.simulate_frames(10)
+	_runner.invoke(STOP)
+	assert_float(_bar.value).is_equal(0.0)
+	assert_float(_get_animation_speed.call()).is_equal(0.0)
+	assert_int(_highlight.frame).is_equal(0)
+
+
+func test_stop_start() -> void:
+	_runner.invoke(START, 0.1)
+	await _runner.simulate_frames(1)
+	_runner.invoke(STOP)
+	await _runner.simulate_frames(1)
+	_runner.invoke(START, 0.1)
+	await _runner.simulate_frames(10)
+	assert_float(_bar.value).is_greater(0.0)
+	assert_float(_get_animation_speed.call()).is_greater(0.0)
+
+
+func test_complete() -> void:
+	_runner.invoke(START, 0.0)
+	await _runner.simulate_frames(10)
+	assert_float(_bar.value).is_equal(_bar.max_value)
+	assert_str(_highlight.animation).is_equal(&"complete")
